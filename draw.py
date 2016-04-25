835f562a839defe52aece17e7dd9c0062dea5feb
Wilton Rodrigues <braynwilton@gmail.com>
2016-04-25 18:51:49 -0300

    Adding new paints

--- draw.py
+++ draw.py
@@ -1,5 +1,9 @@
 class Draw(object):
 
+    def draw_clean(self):
+        arq = open('/home/wilton/decorator.py', 'w')
+        arq.close()
+
     def draw_head(self):
         arq = open('/home/wilton/decorator.py', 'r')
         conteudo = arq.readlines()
@@ -53,6 +57,43 @@ pen.right(90)
 pen.forward(250)
 pen.right(90)
 pen.forward(200)
+pen.right(90)
+pen.pensize(1)
+
+"""
+        arq = open('/home/wilton/decorator.py', 'w')
+        conteudo.append(texto)
+        arq.writelines(conteudo)
+        arq.close()
+
+    def draw_rolagem_vertical(self):
+        arq = open('/home/wilton/decorator.py', 'r')
+        conteudo = arq.readlines()
+        texto = """pen.forward(240)
+pen.right(90)
+pen.forward(200)
+pen.right(90)
+pen.forward(240)
+pen.right(90)
+pen.forward(200)
+pen.right(90)
+
+"""
+        arq = open('/home/wilton/decorator.py', 'w')
+        conteudo.append(texto)
+        arq.writelines(conteudo)
+        arq.close()
+
+    def draw_rolagem_horizontal(self):
+        arq = open('/home/wilton/decorator.py', 'r')
+        conteudo = arq.readlines()
+        texto = """pen.forward(240)
+pen.right(90)
+pen.forward(190)
+pen.right(90)
+pen.forward(240)
+pen.right(90)
+pen.forward(190)
 pen.right(90)
 
 """
