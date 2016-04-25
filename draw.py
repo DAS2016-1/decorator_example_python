9e37c335ff7a06078224e42b226e07dbba1f22d4
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-04-25 20:01:44 -0300

    Generalizin home path

--- draw.py
+++ draw.py
@@ -1,35 +1,42 @@
+import os
+
 class Draw(object):
+    homedir = os.path.expanduser('~')
 
     def draw_clean(self):
-        arq = open('/home/wilton/decorator.py', 'w')
+        path = self.homedir + '/decorator.py'
+        arq = open(path, 'w')
         arq.close()
 
     def draw_head(self):
-        arq = open('/home/wilton/decorator.py', 'r')
+        path = self.homedir + '/decorator.py'
+        arq = open(path, 'r')
         conteudo = arq.readlines()
         texto = """import turtle
 wn = turtle.Screen()
 pen = turtle.Turtle()
 
 """
-        arq = open('/home/wilton/decorator.py', 'w')
+        arq = open(path, 'w')
         conteudo.append(texto)
         arq.writelines(conteudo)
         arq.close()
 
     def draw_end(self):
-        arq = open('/home/wilton/decorator.py', 'r')
+        path = self.homedir + '/decorator.py'
+        arq = open(path, 'r')
         conteudo = arq.readlines()
         texto = """wn.exitonclick()
 
 """
-        arq = open('/home/wilton/decorator.py', 'w')
+        arq = open(path, 'w')
         conteudo.append(texto)
         arq.writelines(conteudo)
         arq.close()
 
     def draw_janela_simples(self):
-        arq = open('/home/wilton/decorator.py', 'r')
+        path = self.homedir + '/decorator.py'
+        arq = open(path, 'r')
         conteudo = arq.readlines()
         texto = """pen.forward(250)
 pen.right(90)
@@ -41,13 +48,14 @@ pen.forward(200)
 pen.right(90)
 
 """
-        arq = open('/home/wilton/decorator.py', 'w')
+        arq = open(path, 'w')
         conteudo.append(texto)
         arq.writelines(conteudo)
         arq.close()
 
     def draw_borda(self):
-        arq = open('/home/wilton/decorator.py', 'r')
+        path = self.homedir + '/decorator.py'
+        arq = open(path, 'r')
         conteudo = arq.readlines()
         texto = """pen.pensize(3)
 pen.forward(250)
@@ -61,13 +69,14 @@ pen.right(90)
 pen.pensize(1)
 
 """
-        arq = open('/home/wilton/decorator.py', 'w')
+        arq = open(path, 'w')
         conteudo.append(texto)
         arq.writelines(conteudo)
         arq.close()
 
     def draw_rolagem_vertical(self):
-        arq = open('/home/wilton/decorator.py', 'r')
+        path = self.homedir + '/decorator.py'
+        arq = open(path, 'r')
         conteudo = arq.readlines()
         texto = """pen.forward(240)
 pen.right(90)
@@ -79,13 +88,14 @@ pen.forward(200)
 pen.right(90)
 
 """
-        arq = open('/home/wilton/decorator.py', 'w')
+        arq = open(path, 'w')
         conteudo.append(texto)
         arq.writelines(conteudo)
         arq.close()
 
     def draw_rolagem_horizontal(self):
-        arq = open('/home/wilton/decorator.py', 'r')
+        path = self.homedir + '/decorator.py'
+        arq = open(path, 'r')
         conteudo = arq.readlines()
         texto = """pen.forward(240)
 pen.right(90)
@@ -97,7 +107,7 @@ pen.forward(190)
 pen.right(90)
 
 """
-        arq = open('/home/wilton/decorator.py', 'w')
+        arq = open(path, 'w')
         conteudo.append(texto)
         arq.writelines(conteudo)
         arq.close()
